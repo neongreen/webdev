db5d235e7d8460386fa14d766dd542e05eaefd52
Artyom Kazak <artyom@artyom.me>
2022-07-16 21:51:22 +0200

    if

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,12 +12,17 @@ export function Navbar() {
             <B.Nav className="me-auto">
               <B.Nav.Link href="/glitch">Glitch</B.Nav.Link>
               <B.NavDropdown title="Основы">
-                <B.NavDropdown.Item href="/lessons/basics/1">1. Введение в HTML</B.NavDropdown.Item>
-                <B.NavDropdown.Item href="/lessons/basics/2">2. Введение в CSS</B.NavDropdown.Item>
+                <B.NavDropdown.Item href="/basics/1">1. Введение в HTML</B.NavDropdown.Item>
+                <B.NavDropdown.Item href="/basics/2">2. Введение в CSS</B.NavDropdown.Item>
+              </B.NavDropdown>
+              <B.NavDropdown title="JavaScript">
+                <B.NavDropdown.Item href="/js/if">
+                  <code>if</code>
+                </B.NavDropdown.Item>
               </B.NavDropdown>
               <B.Nav.Link href="/codewars">Codewars</B.Nav.Link>
               <B.NavDropdown title="Wordle">
-                <B.NavDropdown.Item href="/lessons/wordle/1">1. Первый урок</B.NavDropdown.Item>
+                <B.NavDropdown.Item href="/wordle/1">1. Первый урок</B.NavDropdown.Item>
               </B.NavDropdown>
               <B.NavDropdown title="Next.js">
                 <B.NavDropdown.Item href="/next/init">Как создать проект</B.NavDropdown.Item>
