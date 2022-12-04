83003b5ece6361e52dceb871ba972649be93dee6
Artyom Kazak <artyom@artyom.me>
2022-12-04 22:47:47 +0100

    Footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,12 +1,22 @@
 import { Navbar } from '@components/navbar'
+import { BsGithub, BsTelegram } from 'react-icons/bs'
+import styles from './layout.module.scss'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
-      <main className="container" style={{ paddingBottom: '5rem' }}>
-        {children}
-      </main>
+      <main className={`container ${styles.main}`}>{children}</main>
+      <footer className={`${styles.footer} bg-light`}>
+        <a href="https://github.com/neongreen/webdev" className="d-flex align-items-center">
+          <BsGithub />
+          <span className="ms-1">GitHub</span>
+        </a>
+        <a href="https://t.me/lightgreen" className="d-flex align-items-center">
+          <BsTelegram />
+          <span className="ms-1">Telegram</span>
+        </a>
+      </footer>
     </>
   )
 }
